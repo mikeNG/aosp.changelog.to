9792f7c7fa5f684495f1de337f4224239230e848
Michael Bestas <mkbestas@gmail.com>
2022-07-28 14:37:18 -0400

    Add script to generate changelog from specified tags

---
+++ generate_changelog_from_tag.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PARENT_TAG=$1
+TARGET_TAG=$2
+AOSP_DIRECTORY=$3
+
+WORK_DIRECTORY=$AOSP_DIRECTORY/build/make
+cd $WORK_DIRECTORY
+
+echo "Generating changelog from $PARENT_TAG to $TARGET_TAG"
+
+# Enter the AOSP working directory
+cd $AOSP_DIRECTORY
+
+./get_gitlog.sh $PARENT_TAG $TARGET_TAG
