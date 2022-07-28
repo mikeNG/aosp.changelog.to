f8c832edc4b1520a1b09547f37ebb33b03fb28f5
Michael Bestas <mkbestas@gmail.com>
2022-07-28 14:48:06 -0400

    More fixes

--- generate_changelog_from_parent_tag.sh
+++ generate_changelog_from_parent_tag.sh
@@ -3,6 +3,7 @@
 TARGET_TAG=$1
 AOSP_DIRECTORY=$2
 
+BASE_DIRECTORY=$AOSP_DIRECTORY/../aosp.changelog.to
 WORK_DIRECTORY=$AOSP_DIRECTORY/build/make
 cd $WORK_DIRECTORY
 
@@ -14,4 +15,4 @@ echo "Generating changelog from $PARENT_TAG to $TARGET_TAG"
 # Enter the AOSP working directory
 cd $AOSP_DIRECTORY
 
-$WORK_DIRECTORY/../aosp.changelog.to/get_gitlog.sh $PARENT_TAG $TARGET_TAG
+$BASE_DIRECTORY/get_gitlog.sh $PARENT_TAG $TARGET_TAG $BASE_DIRECTORY
--- generate_changelog_from_tag.sh
+++ generate_changelog_from_tag.sh
@@ -4,6 +4,7 @@ PARENT_TAG=$1
 TARGET_TAG=$2
 AOSP_DIRECTORY=$3
 
+BASE_DIRECTORY=$AOSP_DIRECTORY/../aosp.changelog.to
 WORK_DIRECTORY=$AOSP_DIRECTORY/build/make
 cd $WORK_DIRECTORY
 
@@ -12,4 +13,4 @@ echo "Generating changelog from $PARENT_TAG to $TARGET_TAG"
 # Enter the AOSP working directory
 cd $AOSP_DIRECTORY
 
-$WORK_DIRECTORY/../aosp.changelog.to/get_gitlog.sh $PARENT_TAG $TARGET_TAG
+$BASE_DIRECTORY/get_gitlog.sh $PARENT_TAG $TARGET_TAG $BASE_DIRECTORY
