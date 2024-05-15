3843cabf55d9611c26b6c44f9021a95e2588eafc
Michael Bestas <mikeioannina@gmail.com>
2024-05-15 04:56:24 +0300

    Use 2 dot git log to generate the changelogs
    We only want the new tag commits, not a mix of old and new since
    divergence. We probably need to regenerate all the changelogs again...
    This also allows generating reverse changelogs.

--- get_project_gitlog.sh
+++ get_project_gitlog.sh
@@ -6,7 +6,7 @@ NEW_VERSION=$2
 grep projectname .git/config ;
 if git rev-parse ${OLD_VERSION} >/dev/null 2>&1
 then
-git log --oneline --no-merges ${OLD_VERSION}...${NEW_VERSION}
+git log --oneline --no-merges ${OLD_VERSION}..${NEW_VERSION}
 else
 git log --oneline --no-merges ${NEW_VERSION}
 fi
