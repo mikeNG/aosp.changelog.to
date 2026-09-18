ae4d8dea1a3ae240c97e7b9e36e512773678c21a
Michael Bestas <mkbestas@gmail.com>
2026-09-19 00:51:49 +0300

    Add 17 security tags to index

--- generate_index.sh
+++ generate_index.sh
@@ -36,6 +36,7 @@ function generate_tag_list() {
 cat "${script_dir}/html_templates/index_header.html" > "$output_file"
 
 generate_tag_list "android-17"
+generate_tag_list "android-security-17"
 
 generate_version_divider "16"
 generate_tag_list "android-16"
